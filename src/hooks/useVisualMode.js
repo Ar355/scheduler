b8cc8a8cb5330bb54b86d3689eeb91e102a0378e
Ar355 <zeneli.arian@gmail.com>
2020-09-07 14:35:35 -0400

    add visualMode hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  //const [mode, setMode] = useState(initial);
+
+
+  const [history, setHistory] = useState([initial]);
+  
+  
+  const  transition = (addMode, replace = false)  => {
+    
+    if(!replace) {
+      //const newHistory = history.slice(0, history.length - 1);
+      // setHistory([...newHistory]);
+      setHistory(prevHistory => [...prevHistory, addMode])
+      //setHistory([...history, addMode]);
+      // setMode(addMode);
+      
+    } else {
+
+      setHistory(history => [...history.slice(0, history.length -1), addMode])
+      // setMode(addMode)
+    }
+
+  }
+ 
+  
+
+  const back = () => {
+   
+    if (history.length < 2) {
+      return
+    }
+
+    // if (history.length > 1){
+     
+      // const newHistory = history.slice(0, history.length - 1);
+      setHistory(newHistory => [...newHistory.slice(0, history.length - 1)])
+      // setHistory([...newHistory]);
+      // setMode(history[history.length -1]);
+    // } 
+    // else {
+    //   setMode(mode)
+    // }
+
+  }
+  return { mode: history[history.length - 1 ], transition, back };
+}
+
+
